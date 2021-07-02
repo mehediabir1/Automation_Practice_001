6b481d31ac037f56edbd88392071dc9cdda62ffc
Mehedi Abir <mehedi.abir2@gmail.com>
2021-07-02 21:28:35 +0600

    Added New Feature

--- src/test/java/com/automate/RegistrationAutomate.java
+++ src/test/java/com/automate/RegistrationAutomate.java
@@ -8,9 +8,10 @@ public class RegistrationAutomate extends Base{
     public static void main(String[] args) {
         ChromeOpen();
 //        ChromeClose();
-//        TC_001_Registration_Valid();
-//        TC_002_Registration_inValid();
-        TC_003_Registration_inValid();
+//          TC_001_Registration_Valid();
+//          TC_002_Registration_inValid();
+//          TC_003_Registration_inValid();
+          TC_004_Registration_inValid();
     }
 
     public static void TC_001_Registration_Valid(){
@@ -114,6 +115,47 @@ public class RegistrationAutomate extends Base{
         //step 5
         WebElement Telephone= driver.findElement(By.id("input-telephone"));
         Telephone.sendKeys("abcdeeffas");
+
+        //step 6
+        WebElement Password= driver.findElement(By.id("input-password"));
+        Password.sendKeys("123456");
+
+        //step 7
+        WebElement Password2= driver.findElement(By.id("input-confirm"));
+        Password2.sendKeys("123456");
+
+        //Checkbox
+        WebElement CheckBox= driver.findElement(By.name("agree"));
+        CheckBox.click();
+
+        //final step
+        WebElement Reg_Btn= driver.findElement(By.cssSelector(".btn-primary"));
+        Reg_Btn.click();
+
+        //logout
+        WebElement logout= driver.findElement(By.linkText("Logout"));
+        logout.click();
+    }
+
+    public static void TC_004_Registration_inValid(){
+        //step 1
+        driver.get("https://demo.opencart.com/index.php?route=account/register");
+
+        //step 2
+        WebElement fName= driver.findElement(By.id("input-firstname"));
+        fName.sendKeys(" ");
+
+        //step 3
+        WebElement lName= driver.findElement(By.id("input-lastname"));
+        lName.sendKeys(" ");
+
+        //step 4
+        WebElement Email= driver.findElement(By.id("input-email"));
+        Email.sendKeys("abcdmail@mail.com");
+
+        //step 5
+        WebElement Telephone= driver.findElement(By.id("input-telephone"));
+        Telephone.sendKeys("123456789");
 
         //step 6
         WebElement Password= driver.findElement(By.id("input-password"));
